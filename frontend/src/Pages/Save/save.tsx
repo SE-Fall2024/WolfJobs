cdf9d898f54dcf6b71a1d4003923bc4cfaf9c066
Shanmuka Venkata Sai Sathwik Palakurty <72595159+sathwik02022002@users.noreply.github.com>
2024-11-26 19:13:43 -0500

    Update save.tsx

--- frontend/src/Pages/Save/save.tsx
+++ frontend/src/Pages/Save/save.tsx
@@ -32,8 +32,8 @@ const Saved = () => {
             jobsList={filteredJobList}
             title={"Saved Applications"}
           />
+          <JobDetailView />
         </div>
-        <JobDetailView />
       </div>
     </>
   );
